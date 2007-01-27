bf161ef2bf05dcc90895d9057fc21b05adaf9ed4
Christian Neumair <cneumair@src.gnome.org>
2007-01-27 20:35:15 +0000

    Add dummy ChangeLog if it doesn't exist at autogen.sh time.
    svn path=/trunk/; revision=122

--- autogen.sh
+++ autogen.sh
@@ -7,6 +7,10 @@ test -z "$srcdir" && srcdir=.
 PKG_NAME="nautilus-open-terminal"
 REQUIRED_AUTOMAKE_VERSION=1.7
 
+(test -f $srcdir/ChangeLog) || {
+    echo -n "Dummy Makefile (to satisfy automake)" > $srcdir/ChangeLog
+}
+
 (test -f $srcdir/configure.in) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
