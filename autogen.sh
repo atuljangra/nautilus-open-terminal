cb0ee3752b34a8c159422262c35d776ff81b045b
Wouter Bolsterlee <wbolster@src.gnome.org>
2008-02-28 22:55:11 +0000

    Add dummy ChangeLog file with a notice that no ChangeLog file is used. Remove ChangeLog generation (with incorrect message btw) from autogen.sh.
    svn path=/trunk/; revision=171

--- autogen.sh
+++ autogen.sh
@@ -7,16 +7,11 @@ test -z "$srcdir" && srcdir=.
 PKG_NAME="nautilus-open-terminal"
 REQUIRED_AUTOMAKE_VERSION=1.7
 
-(test -f $srcdir/ChangeLog) || {
-    echo -n "Dummy Makefile (to satisfy automake)" > $srcdir/ChangeLog
-}
-
 (test -f $srcdir/configure.in) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
 }
-
 
 which gnome-autogen.sh || {
     echo "You need to install gnome-common from the GNOME CVS"
